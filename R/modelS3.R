63bd1e16faaacddfd9169b4f6b46f11e71fa61a1
Jackie Li <jackieli@smu.edu.sg>
2026-04-28 17:10:02 +0000

    version 0.1.3

--- R/modelS3.R
+++ R/modelS3.R
@@ -1,15 +1,17 @@
 #' Common S3 methods for stochastic mortality models
 #'
 #' @description
-#' Common S3 methods for objects returned by \code{LCS()}, \code{RHS()}, \code{APCS()}, \code{CBDS()}, \code{CBDCS()}, \code{CBDQCS()}, and \code{STARS()}:
+#' Common S3 methods for objects returned by \code{LCS()}, \code{RHS()}, \code{APCS()}, \code{CBDS()}, \code{CBDCS()}, \code{CBDQCS()}, \code{STARS()}, \code{ENS()}, \code{ENI()}, \code{CFMS()}, \code{CFM2S()}, and \code{CAES()}:
 #'
 #' coef(object) gives estimated parameter values.
 #'
-#' forecast::forecast(object) gives mortality forecasts smoothed by selected mortality curve.
+#' forecast::forecast(object) gives mortality forecasts smoothed by selected mortality curve (or ensemble interval forecasts for \code{ENI()}).
 #'
-#' plot(object) plots estimated parameter values, standardised residuals heatmap, data, and mortality forecasts smoothed by selected mortality curve.
+#' plot(object) plots estimated parameter values, standardised residuals heatmap, data, and mortality forecasts smoothed by selected mortality curve (or ensemble interval forecasts for \code{ENI()}).
 #'
 #' residuals(object) gives standardised residuals on \eqn{ln(m_{x,t})}.
+#'
+#' simulate(object,nsim,seed) gives simulated future mortality rates (unsmoothed).
 #'
 #' @name modelS3
 #'
@@ -20,6 +22,9 @@
 #' @method coef CBDCS
 #' @method coef CBDQCS
 #' @method coef STARS
+#' @method coef CFMS
+#' @method coef CFM2S
+#' @method coef CAES
 #'
 #' @method forecast LCS
 #' @method forecast RHS
@@ -29,6 +34,10 @@
 #' @method forecast CBDQCS
 #' @method forecast STARS
 #' @method forecast ENS
+#' @method forecast ENI
+#' @method forecast CFMS
+#' @method forecast CFM2S
+#' @method forecast CAES
 #'
 #' @method plot LCS
 #' @method plot RHS
@@ -38,6 +47,10 @@
 #' @method plot CBDQCS
 #' @method plot STARS
 #' @method plot ENS
+#' @method plot ENI
+#' @method plot CFMS
+#' @method plot CFM2S
+#' @method plot CAES
 #'
 #' @method residuals LCS
 #' @method residuals RHS
@@ -46,5 +59,16 @@
 #' @method residuals CBDCS
 #' @method residuals CBDQCS
 #' @method residuals STARS
+#' @method residuals CFMS
+#' @method residuals CFM2S
+#' @method residuals CAES
+#'
+#' @method simulate LCS
+#' @method simulate RHS
+#' @method simulate APCS
+#' @method simulate CBDS
+#' @method simulate CBDCS
+#' @method simulate CBDQCS
+#' @method simulate STARS
 #'
 NULL
